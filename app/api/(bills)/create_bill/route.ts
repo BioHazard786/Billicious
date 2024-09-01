662f8b9f4b2f6687bf43d7245521da939ab27034
K0DEL <kesav.singh6@gmail.com>
2024-09-01 21:02:00 +0530

    added validations for bill route

--- app/api/(bills)/create_bill/route.ts
+++ app/api/(bills)/create_bill/route.ts
@@ -5,12 +5,25 @@ export const POST = async (request: Request) => {
   let bill;
   try {
     const requestData = await request.json();
+
+    if (requestData.group_id === undefined) {
+      throw new Error("GroupId is Required");
+    }
+    if (requestData.name === undefined) {
+      throw new Error("Bill Name is Required");
+    }
+    if (requestData.drawees === undefined || requestData.payees === undefined) {
+      throw new Error("Drawees and Payees Required");
+    }
+
     bill = await createBillInDB(requestData);
   } catch (err) {
-    console.log(err);
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message }, { status: 400 });
+    }
     return NextResponse.json(
       { message: "Something went Wrong" },
-      { status: 400 },
+      { status: 500 },
     );
   }
   return NextResponse.json({ bill }, { status: 200 });
--- app/api/(bills)/view_bill/route.ts
+++ app/api/(bills)/view_bill/route.ts
@@ -5,12 +5,19 @@ export const POST = async (request: Request) => {
   let bill;
   try {
     const requestData = await request.json();
+
+    if (requestData.bill_id === undefined) {
+      throw new Error("BillId is Required");
+    }
+
     bill = await getBillFromDB(requestData);
   } catch (err) {
-    console.log(err);
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message }, { status: 400 });
+    }
     return NextResponse.json(
       { message: "Something went Wrong" },
-      { status: 400 },
+      { status: 500 },
     );
   }
   return NextResponse.json({ bill }, { status: 200 });
