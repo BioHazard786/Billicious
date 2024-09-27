bdde5e4d5fe83d28230ee42cfb2a9114c7facdaa
BioHazard786 <bzatch70@gmail.com>
2024-09-27 22:48:23 +0530

    added authentication

---
+++ app/group/[slug]/members/loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Loader } from "lucide-react";
+
+const Loading = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center [@supports(height:100dvh)]:h-dvh">
+      <Loader className="size-8 animate-spin text-muted-foreground" />
+    </div>
+  );
+};
+
+export default Loading;
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,25 +1,25 @@
-// "use client";
+"use client";
 
-// import LoadingAnimationDark from "@/public/LoadingAnimDark.json";
-// import LoadingAnimationLight from "@/public/LoadingAnimLight.json";
-// import Lottie from "lottie-react";
-import { Loader } from "lucide-react";
-// import { useTheme } from "next-themes";
-// import { useMemo } from "react";
+import LoadingAnimationDark from "@/public/LoadingAnimDark.json";
+import LoadingAnimationLight from "@/public/LoadingAnimLight.json";
+import Lottie from "lottie-react";
+// import { Loader } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useMemo } from "react";
 
 const Loading = () => {
-  // const { resolvedTheme } = useTheme();
+  const { resolvedTheme } = useTheme();
 
-  // const animationData = useMemo(
-  //   () =>
-  //     resolvedTheme === "light" ? LoadingAnimationLight : LoadingAnimationDark,
-  //   [resolvedTheme],
-  // );
+  const animationData = useMemo(
+    () =>
+      resolvedTheme === "light" ? LoadingAnimationLight : LoadingAnimationDark,
+    [resolvedTheme],
+  );
 
   return (
     <div className="flex h-screen w-full items-center justify-center [@supports(height:100dvh)]:h-dvh">
-      {/* <Lottie animationData={animationData} loop={true} className="size-16" /> */}
-      <Loader className="size-8 animate-spin text-muted-foreground" />
+      <Lottie animationData={animationData} loop={true} className="size-16" />
+      {/* <Loader className="size-8 animate-spin text-muted-foreground" /> */}
     </div>
   );
 };
